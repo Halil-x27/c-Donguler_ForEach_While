f5d679d2f450c0aea437293ea1614c7358757e6f
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-08 15:29:12 +0300

    Add files via upload

---
+++ Donguler_ForEach_While/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Donguler_ForEach
+{
+ class Program
+ {
+    public static void Main(string[] args)
+    {
+        // while
+        Console.WriteLine("sayi giriniz");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+        while (sayac <= sayi)
+        {
+            toplam+= sayac;
+            sayac ++;
+        }
+
+        Console.WriteLine(toplam/sayac);
+
+
+        //forEach
+
+        string[] arabalar = {"bmw", "nissan","audi","woksvagen","opel","tofas"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+ }   
+}
